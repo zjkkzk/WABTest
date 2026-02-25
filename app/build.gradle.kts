7a0e201d46baff89dc228e7b688e85f7db04ee02
hd <hdshare@vip.qq.com>
2026-02-26 07:57:22 +0800

    bump version 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "me.hd.wexpt"
         minSdk = 27
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
         buildConfigField("String", "APP_NAME", "\"WExpt\"")
     }
 
